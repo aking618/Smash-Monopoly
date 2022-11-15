978ee715e12806650359c73656f1c68cbf099d8e
Ayren King <ashsquads@gmail.com>
2022-11-15 12:47:40 -0600

    AK : hide fight popup until actions are complete

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -9,84 +9,88 @@ import FreeCharacterPopup from "../FreeCharacterPopup/FreeCharacterPopup";
 import WinnerPopup from "../WinnerPopup/WinnerPopup";
 import TaxPopup from "../TaxPopup/TaxPopup";
 import BannedPopup from "../BannedPopup/BannedPopup";
+import { AllPopupsHandled } from "../../Game/utility";
 
 const SmashMonopolyBoard = ({ ctx, G, moves }) => {
-  const handleStealCharacter = (pos) => moves.stealCharacter(pos);
-  const handleWinner = (playerID) => moves.pickWinner(playerID);
-  const handlePickFreeCharacter = (pos) => moves.pickFreeCharacter(pos);
-  const handleAcceptTaxEffect = () => moves.acceptTaxEffect();
-  const handleAcceptBanned = () => moves.acceptBanned();
+    const handleStealCharacter = (pos) => moves.stealCharacter(pos);
+    const handleWinner = (playerID) => moves.pickWinner(playerID);
+    const handlePickFreeCharacter = (pos) => moves.pickFreeCharacter(pos);
+    const handleAcceptTaxEffect = () => moves.acceptTaxEffect();
+    const handleAcceptBanned = () => moves.acceptBanned();
 
-  return (
-    <div className="container">
-      {G.showFightPopup && (
-        <FightPopup
-          G={G}
-          p1Fighter={boardInfo
-            .filter((n) => n)
-            .find((tile) => tile.pos === G.player1Pos)}
-          p2Fighter={boardInfo
-            .filter((n) => n)
-            .find((tile) => tile.pos === G.player2Pos)}
-          handleWinner={(playerId) => {
-            handleWinner(playerId);
-          }}
-        />
-      )}
-      <PlayerControls G={G} ctx={ctx} moves={moves} />
+    return (
+        <div className="container">
+            {G.showFightPopup && AllPopupsHandled(G) && (
+                <FightPopup
+                    G={G}
+                    p1Fighter={boardInfo
+                        .filter((n) => n)
+                        .find((tile) => tile.pos === G.player1Pos)}
+                    p2Fighter={boardInfo
+                        .filter((n) => n)
+                        .find((tile) => tile.pos === G.player2Pos)}
+                    handleWinner={(playerId) => {
+                        handleWinner(playerId);
+                    }}
+                />
+            )}
+            <PlayerControls G={G} ctx={ctx} moves={moves} />
 
-      {G.showStealPopup && (
-        <StealCharacterPopup
-          G={G}
-          matchWinner={G.matchWinner}
-          handleStealCharacter={(pos) => {
-            handleStealCharacter(pos);
-          }}
-        />
-      )}
-      {G.showTaxPopup && (
-        <TaxPopup acceptPopup={() => handleAcceptTaxEffect()} />
-      )}
-      {G.showSelectFreeCharacterPopup && (
-        <FreeCharacterPopup
-          G={G}
-          ctx={ctx}
-          handleResult={(pos) => {
-            handlePickFreeCharacter(pos);
-          }}
-        />
-      )}
-      {G.showBannedPopup && (
-        <BannedPopup ctx={ctx} acceptPopup={() => handleAcceptBanned()} />
-      )}
-      {ctx.gameover && <WinnerPopup ctx={ctx} />}
-      <div className="boardContainer">
-        {boardInfo.map((tile, index) => {
-          if (tile === null) {
-            return <div className="filler" key={index} />;
-          }
+            {G.showStealPopup && (
+                <StealCharacterPopup
+                    G={G}
+                    matchWinner={G.matchWinner}
+                    handleStealCharacter={(pos) => {
+                        handleStealCharacter(pos);
+                    }}
+                />
+            )}
+            {G.showTaxPopup && (
+                <TaxPopup acceptPopup={() => handleAcceptTaxEffect()} />
+            )}
+            {G.showSelectFreeCharacterPopup && (
+                <FreeCharacterPopup
+                    G={G}
+                    ctx={ctx}
+                    handleResult={(pos) => {
+                        handlePickFreeCharacter(pos);
+                    }}
+                />
+            )}
+            {G.showBannedPopup && (
+                <BannedPopup
+                    ctx={ctx}
+                    acceptPopup={() => handleAcceptBanned()}
+                />
+            )}
+            {ctx.gameover && <WinnerPopup ctx={ctx} />}
+            <div className="boardContainer">
+                {boardInfo.map((tile, index) => {
+                    if (tile === null) {
+                        return <div className="filler" key={index} />;
+                    }
 
-          return (
-            <Tile
-              key={index}
-              image={tile.image}
-              strip={tile.strip}
-              stripColor={tile.stripColor}
-              top={tile.top}
-              left={tile.left}
-              right={tile.right}
-              title={tile.title}
-              corner={tile.corner}
-              cornerRotate={tile.cornerRotate}
-              player1Pos={tile.pos === G.player1Pos}
-              player2Pos={tile.pos === G.player2Pos}
-              ownedBy={G.cells[tile.pos]}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+                    return (
+                        <Tile
+                            key={index}
+                            image={tile.image}
+                            strip={tile.strip}
+                            stripColor={tile.stripColor}
+                            top={tile.top}
+                            left={tile.left}
+                            right={tile.right}
+                            title={tile.title}
+                            corner={tile.corner}
+                            cornerRotate={tile.cornerRotate}
+                            player1Pos={tile.pos === G.player1Pos}
+                            player2Pos={tile.pos === G.player2Pos}
+                            ownedBy={G.cells[tile.pos]}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default SmashMonopolyBoard;
--- src/components/FightPopup/FightPopup.jsx
+++ src/components/FightPopup/FightPopup.jsx
@@ -3,39 +3,49 @@ import Tile from "../Tile/Tile";
 import "./styles.css";
 
 const FightPopup = ({ G, p1Fighter, p2Fighter, handleWinner }) => {
-  return (
-    <div className="popup-container">
-      <h2>FIGHT!</h2>
-      <p>Player One will record the results of the match.</p>
+    return (
+        <div className="popup-container">
+            <h2>FIGHT!</h2>
+            <p>Player One will record the results of the match.</p>
 
-      <div className="fighter-options">
-        <div className="fighter-container">
-          <Tile
-            image={p1Fighter.image}
-            strip={p1Fighter.strip}
-            stripColor={p1Fighter.stripColor}
-            title={p1Fighter.title}
-            ownedBy={G.cells[p1Fighter.pos]}
-          />
-          <button className="fighter-btn" onClick={() => handleWinner("0")}>
-            Winner
-          </button>
+            <div className="fighter-options">
+                <div className="fighter-container">
+                    <div className="fighter-tile">
+                        <Tile
+                            image={p1Fighter.image}
+                            strip={p1Fighter.strip}
+                            stripColor={p1Fighter.stripColor}
+                            title={p1Fighter.title}
+                            ownedBy={G.cells[p1Fighter.pos]}
+                        />
+                    </div>
+                    <button
+                        className="fighter-btn"
+                        onClick={() => handleWinner("0")}
+                    >
+                        Winner
+                    </button>
+                </div>
+                <div className="fighter-container">
+                    <div className="fighter-tile">
+                        <Tile
+                            image={p2Fighter.image}
+                            strip={p2Fighter.strip}
+                            stripColor={p2Fighter.stripColor}
+                            title={p2Fighter.title}
+                            ownedBy={G.cells[p2Fighter.pos]}
+                        />
+                    </div>
+                    <button
+                        className="fighter-btn"
+                        onClick={() => handleWinner("1")}
+                    >
+                        Winner
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="fighter-container">
-          <Tile
-            image={p2Fighter.image}
-            strip={p2Fighter.strip}
-            stripColor={p2Fighter.stripColor}
-            title={p2Fighter.title}
-            ownedBy={G.cells[p2Fighter.pos]}
-          />
-          <button className="fighter-btn" onClick={() => handleWinner("1")}>
-            Winner
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FightPopup;
